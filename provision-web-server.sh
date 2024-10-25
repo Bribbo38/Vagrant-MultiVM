da966f1136603220b42b930a9daffe512dbe9898
Bribbo38 <luca0gastaldi@gmail.com>
2024-10-25 11:21:05 +0200

    Updated provisioning

--- provision-web-server.sh
+++ provision-web-server.sh
@@ -6,7 +6,3 @@ apt-get upgrade -y
 
 # Installing Apache2 and PHP with all the modules to make the site work
 apt-get install apache2 php libapache2-mod-php php-mysql php-mysqli php-pdo php-pdo-mysql -y
-
-# Restarting and making sure the Apache service is working
-sudo systemctl restart apache2
-sudo systemctl enable apache2

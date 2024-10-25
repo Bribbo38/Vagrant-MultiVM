ed8f3b194917c4764d704635ec75f99c1cccf701
Bribbo38 <105216554+Bribbo38@users.noreply.github.com>
2024-10-25 11:38:49 +0200

    Update provision-db-server.sh

--- provision-db-server.sh
+++ provision-db-server.sh
@@ -4,18 +4,18 @@
 apt-get install mysql-server -y
 
 # Setting the MySQL binding to all IP's and skipping the name resolving
-echo "bind-address = 0.0.0.0" | sudo tee -a /etc/mysql/mysql.conf.d/mysqld.cnf
-echo "skip-name-resolve" | sudo tee -a /etc/mysql/mysql.conf.d/mysqld.cnf
+echo "bind-address = 0.0.0.0" | tee -a /etc/mysql/mysql.conf.d/mysqld.cnf
+echo "skip-name-resolve" | tee -a /etc/mysql/mysql.conf.d/mysqld.cnf
 
 # Restarting the MySQL service
-sudo systemctl restart mysql
+systemctl restart mysql
 
 # Creating the 'webserver' user and giving all the permissions
-sudo mysql -e "CREATE USER 'webserver'@'10.10.20.10' IDENTIFIED BY 'Password123';"
-sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'webserver'@'10.10.20.10';"
-sudo mysql -e "FLUSH PRIVILEGES;"
+mysql -e "CREATE USER 'webserver'@'10.10.20.10' IDENTIFIED BY 'Password123';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'webserver'@'10.10.20.10';"
+mysql -e "FLUSH PRIVILEGES;"
 
 # Creating Database with a table and some data
-sudo mysql -e "CREATE DATABASE test_db;"
-sudo mysql -e "CREATE TABLE test_db.users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(25) NOT NULL, surname VARCHAR(35) NOT NULL);"
-sudo mysql -e "INSERT INTO test_db.users(name, surname) VALUES ('Tom', 'Schillerwein'),('Matteo', 'Terranova'),('Nicolas', 'Ghiggi'),('Keisy', 'Rodriguez');"
+mysql -e "CREATE DATABASE test_db;"
+mysql -e "CREATE TABLE test_db.users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(25) NOT NULL, surname VARCHAR(35) NOT NULL);"
+mysql -e "INSERT INTO test_db.users(name, surname) VALUES ('Tom', 'Schillerwein'),('Matteo', 'Terranova'),('Nicolas', 'Ghiggi'),('Keisy', 'Rodriguez');"

db7a36dcd215456db87f8bd35c5dd623a2b809e5
Bribbo38 <luca0gastaldi@gmail.com>
2024-10-25 09:01:01 +0200

    Updated DB provisioning and added .gitignore

--- provision-db-server.sh
+++ provision-db-server.sh
@@ -22,3 +22,8 @@ sudo systemctl restart mysql
 sudo mysql -e "CREATE USER 'webserver'@'10.10.20.10' IDENTIFIED BY 'Password123';"
 sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'webserver'@'10.10.20.10';"
 sudo mysql -e "FLUSH PRIVILEGES;"
+
+# Creating Database with a table and some data
+sudo mysql -e "CREATE DATABASE test_db;"
+sudo mysql -e "CREATE TABLE test_db.users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(25) NOT NULL, surname VARCHAR(35) NOT NULL);"
+sudo mysql -e "INSERT INTO test_db.users(name, surname) VALUES ('Tom', 'Schillerwein'),('Matteo', 'Terranova'),('Nicolas', 'Ghiggi'),('Keisy', 'Rodriguez');"
